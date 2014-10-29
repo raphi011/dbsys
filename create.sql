1acb16df5564b81f8dc37f7f6f24692460924161
Raphael Gruber <raphi011@gmail.com>
2014-10-29 23:47:19 +0100

    version two

--- create.sql
+++ create.sql
@@ -6,7 +6,8 @@ anschrift varchar(128),
 name varchar(128));
 
 create table dbs.patient (
-person integer references dbs.person(svnr) not null primary key,
+id integer not null primary key,
+person integer references dbs.person(svnr) not null,
 geheilt boolean not null default false,
 seit date not null default CURRENT_DATE);
 
@@ -21,26 +22,40 @@ mitarbeiter integer references dbs.mitarbeiter(person) not null,
 honorar double precision not null,
 monat smallint not null,
 jahr smallint not null,
-primary key(id, mitarbeiter));
+primary key(mitarbeiter, monat, jahr));
 
 create table dbs.arzt (
 person integer references dbs.person(svnr) not null primary key);
 
 create table dbs.krankenhaus (
-name varchar(128) not null primary key,
+id integer not null primary key,
+name varchar(128) not null,
 anschrift varchar(128) not null,
 geleitet_von integer references dbs.mitarbeiter(person) not null);
 
 create table dbs.abteilung (
 name varchar(128) not null,
-krankenhaus varchar(128) references dbs.krankenhaus(name) not null,
+krankenhaus integer references dbs.krankenhaus(id) not null,
 anschrift varchar(128) not null,
 koordiniert_von integer references dbs.mitarbeiter(person) not null,
+
 primary key(name, krankenhaus));
+
+create table dbs.arbeitet (
+mitarbeiter integer references dbs.mitarbeiter(person) not null unique,
+abteilung varchar(128) not null,
+krankenhaus integer not null,
+foreign key (abteilung, krankenhaus) references abteilung (name, krankenhaus));
 
 create table dbs.krankheit (
 name varchar(128) not null primary key,
 bonus real not null);
+
+create table dbs.spezialisiert (
+abteilung varchar(128) not null, 
+krankenhaus integer not null,
+krankheit varchar(128) not null, 
+foreign key (abteilung, krankenhaus) references abteilung (name, krankenhaus));
 
 create table dbs.klasse (
 name varchar(128) not null primary key,
@@ -54,14 +69,13 @@ primary key(krankheit, klasse));
 create table dbs.behandelt (
 arzt integer references dbs.arzt(person) not null,
 krankheit varchar(128) references dbs.krankheit(name) not null,
-patient integer references dbs.patient(person) not null,
-dauer timestamp not null,
-abgerechnet boolean not null default false,
-primary key(krankheit, patient));
+patient integer references dbs.patient(id) not null primary key,
+dauer smallint not null,
+abgerechnet boolean not null default false);
 
 create table dbs.akteneintrag (
-person varchar(128) references dbs.person(name) not null,
-krankenhaus varchar(128) references dbs.krankenhaus(name) not null,
+person integer references dbs.person(svnr) not null,
+krankenhaus integer references dbs.krankenhaus(id) not null,
 krankheit varchar(128) references dbs.krankheit(name) not null,
 von date not null,
 bis date not null);
