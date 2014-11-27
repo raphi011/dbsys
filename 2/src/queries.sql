2d21428a1f692e30650b25530969f71b77babfd0
Raphael Gruber <raphi011@gmail.com>
2014-11-27 18:52:44 +0100

    finished query 1

--- 2/src/queries.sql
+++ 2/src/queries.sql
@@ -1,5 +1,13 @@
-/* Query 1 */
-# TODO Write Query 1 here 
+﻿/* Query 1 */
+WITH RECURSIVE classes(name, uebergeordnet, level) as (
+	SELECT k.name, (select name from klasse where kl_id = k.uebergeordnet), 0 from klasse k where name = 'Herz'
+	UNION ALL
+	SELECT k.name, (select name from klasse where kl_id = k.uebergeordnet), c.level + 1
+	FROM classes c, klasse k 
+	WHERE k.name = c.uebergeordnet
+	)
+ SELECT * from classes;
+
 
 /* Query 2 */
 # TODO Write Query 2 here 
