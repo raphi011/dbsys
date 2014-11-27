c73328c8d6ec312348bd0e3d90f23e96d7ff3cad
Raphael Gruber <raphi011@gmail.com>
2014-11-27 16:27:26 +0100

    implemented view

--- 2/src/view.sql
+++ 2/src/view.sql
@@ -1,5 +1,6 @@
-/* View 1 */
-# TODO Write View 1 here 
-
-
-
+create or replace view PatAbt as select arbeitet_abt_id as abt_id, arbeitet_kh_id as _kh_id, count(*) as patients from (select distinct m.arbeitet_abt_id, m.arbeitet_kh_id, b.patient
+		from behandlung b 
+		inner join patient p on p.svnr = b.patient 
+		inner join mitarbeiter m on m.svnr = b.arzt 
+		where 	p.geheilt = false) as results
+		group by arbeitet_abt_id,arbeitet_kh_id; 
